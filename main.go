8c612fdc7bd0e191900a559a56049df421b6a646
Nik Swap <nikswap@local.host>
2024-12-26 15:14:30 +0100

    Added initial file

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"net"
+	"strconv"
+	"time"
+)
+
+func main() {
+	for port := 1024; port < 30000; port++ {
+		go start_to_listen(":" + strconv.Itoa(port))
+	}
+	for {
+	}
+}
+
+func start_to_listen(port string) {
+	ln, err := net.Listen("tcp", port)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Listeing on port", port)
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		go handleConnection(conn, port)
+	}
+}
+
+func handleConnection(conn net.Conn, port string) {
+	defer conn.Close()
+	fmt.Println("DATA ON PORT", port)
+
+	buf := make([]byte, 1024)
+	timeoutDuration := 60 * time.Second
+	conn.SetReadDeadline(time.Now().Add(timeoutDuration))
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Received: %s", base64.StdEncoding.EncodeToString(buf))
+}
